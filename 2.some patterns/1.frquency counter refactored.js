397a03e7d93cdd6f734dd91843bff69a17946a59
chamman sahu <chammansahu@gmail.com>
2021-03-04 09:50:07 +0530

    'refactored succesfully'

--- 2.some patterns/1.frquency counter refactored.js
+++ 2.some patterns/1.frquency counter refactored.js
@@ -1,19 +1,28 @@
 //uses objects or sets to collect values/frquencies of values
 //often avoid nested loop or O(n^2) operation  with rrays/strings
+//refactor frequecy counter
 
 const same=(arr1,arr2)=>{
    if(arr1.length!=arr2.length){
        return false;
    }
-   else{
-       for(let i in arr1){
-           let correctIndex=arr2.indexOf(arr1[i]**2);
-           if(correctIndex===-1){
-              return false
-           }
+   let freCounter1={},freCounter2={};
+   for (const i of arr1) {
+       freCounter1[i]=(freCounter1[i]||0)+1;
+   }
+   for (const j of arr2) {
+    freCounter2[j]=(freCounter2[j]||0)+1;
+   }
+   for (const key in freCounter1) {
+       if (!(key**2 in freCounter2)) {
+           return false
            
        }
+       if (freCounter2[key**2]!=freCounter1[key]) {
+          return false; 
+       }
    }
+   
    return true;
 } 
 console.log(same([1,2,3,4],[1,4,9,16]))
